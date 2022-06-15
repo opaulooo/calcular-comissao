ad260ae642d5695ce8bbc3a8275444992a0f1a0e
opaulooo <o.paulooo.victor@gmail.com>
2022-06-15 18:44:55 -0300

    feat: jest para testar rota da api e rota do calculo de comissoes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 
 const app = express()
 const port = 8100
-module.exports = port
 
 let comissaoRoute = require('./src/routes/comissao.routes')
 let bodyParser = require('body-parser');
@@ -10,6 +9,13 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use('/api', comissaoRoute)
 
-app.listen(port, () => {
-    console.log('\n\nAPI na porta ' + port + '...\n');
-});
+app.get('/', (req, res) => {
+    res.send({
+        message: 'API no ar!'
+    })
+})
+
+module.exports = {
+    app,
+    port
+}

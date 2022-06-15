90e1d05be19ff10e2337634ee56a935356b1f24a
opaulooo <o.paulooo.victor@gmail.com>
2022-06-15 19:08:28 -0300

    fix: removido rota get calculo comissao

--- src/routes/comissao.routes.js
+++ src/routes/comissao.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { postCalculaComissao, getCalculaComissao } = require("../controllers/calcula-comissao")
+const {
+    postCalculaComissao
+} = require("../controllers/calcula-comissao")
 
 
-router.get('/calcula-comissao', getCalculaComissao)
 router.post('/calcula-comissao', postCalculaComissao)
 
 
